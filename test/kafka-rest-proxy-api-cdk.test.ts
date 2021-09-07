939517a332ca52aba9af8c3ffbac1501e698ff1c
Anupam Gupta <bittu.anupam@gmail.com>
2021-09-07 12:03:19 +0300

    Initial commit

---
+++ test/kafka-rest-proxy-api-cdk.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as KafkaRestProxyApiCdk from '../lib/kafka-rest-proxy-api-cdk-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new KafkaRestProxyApiCdk.KafkaRestProxyApiCdkStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
